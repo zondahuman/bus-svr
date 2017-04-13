c72acd5d98998956d60e8ccd214d41a79f448656
zondahuman <zondahuman@gmail.com>
2017-04-13 19:21:13 +0800

    add

---
+++ bus-disruptor/src/main/java/com/abin/lee/bus/disruptor/event/BusEvent.java
@@ -0,0 +1,17 @@
+package com.abin.lee.bus.disruptor.event;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by abin on 2017/4/13 19:16.
+ * bus-svr
+ * com.abin.lee.bus.disruptor.event
+ */
+@Getter
+@Setter
+public class BusEvent {
+    private String busName;
+
+}
